fc9b9e9f65f598b1281b0ca714b3189e21a1ac33
David <davidx1322@gmail.com>
2026-07-06 13:47:43 -0400

    visualziations and splits

--- data-splits/split generation.py
+++ data-splits/split generation.py
@@ -2,30 +2,28 @@ import pandas as pd
 from datasets import load_dataset
 from collections import defaultdict
 
-from torch import load
+# # 20 questions over 20 subjects, 5 splits
+# dataset = load_dataset("openlifescienceai/medmcqa", split="train")
+# shuffled_dataset = dataset.shuffle(seed=12)
 
-# 20 questions over 20 subjects, 5 splits
-dataset = load_dataset("openlifescienceai/medmcqa", split="train")
-shuffled_dataset = dataset.shuffle(seed=12)
+# questions = defaultdict(list)
 
-questions = defaultdict(list)
-
-for q in shuffled_dataset:
-    if (sum(len(v) for v in questions.values()) > 2000):
-        break
-    if len(questions[q["subject_name"]]) < 100 and q["subject_name"] != "Unknown":
-        questions[q["subject_name"]].append(q)
+# for q in shuffled_dataset:
+#     if (sum(len(v) for v in questions.values()) > 2000):
+#         break
+#     if len(questions[q["subject_name"]]) < 100 and q["subject_name"] != "Unknown":
+#         questions[q["subject_name"]].append(q)
     
 
-for i in range(5):
-    # File generation
-    split = [q for subject, qs in questions.items() for q in qs[i*20:(i+1)*20]]
-    df_split = pd.DataFrame(split, columns=["id", "question", "opa", "opb", "opc", "opd", "cop", "choice_type", "exp", "subject_name", "topic_name"])
-    df_split.to_csv(f"medmcaq_{i+1}.csv", index=False)
+# for i in range(5):
+#     # File generation
+#     split = [q for subject, qs in questions.items() for q in qs[i*20:(i+1)*20]]
+#     df_split = pd.DataFrame(split, columns=["id", "question", "opa", "opb", "opc", "opd", "cop", "choice_type", "exp", "subject_name", "topic_name"])
+#     df_split.to_csv(f"medmcaq_{i+1}.csv", index=False)
 
-    # Check questions per subject
-    subject_counts = df_split["subject_name"].value_counts()
-    print(f"Split {i+1} subject counts:\n{subject_counts}\n")
+#     # Check questions per subject
+#     subject_counts = df_split["subject_name"].value_counts()
+#     print(f"Split {i+1} subject counts:\n{subject_counts}\n")
 
 # 20 questions over 12 subjects, 5 splits
 subjects = [
@@ -46,17 +44,19 @@ questions = [[] for i in range(5)]
 for s in subjects:
     dataset = load_dataset("ekacare/mmlu-medical-mcqs-evaluation-dataset", s, split = "test")
     shuffled_dataset = dataset.shuffle(seed=12)
-    for i in range(5):
-        questions[i].extend(shuffled_dataset[i*20:(i+1)*20])
-    for i in range(5):
-        for j in range(20):
-            questions[i][j]["subject"] = s
+    print(shuffled_dataset)
+    for i in range(100):
+        questions[i % 5].append(shuffled_dataset[i])
+    # for i in range(5):
+    #     for j in range(20):
+    #         questions[i][j]["subject"] = s
+print(questions)
 
-for i in range(5):
-    # File generation
-    df_split = pd.DataFrame(questions[i], columns=["centerpiece", "options", "correct_options", "correct_options_idx", "correct_options_literal", "subject"])
-    df_split.to_csv(f"mmlu_{i+1}.csv", index=False)
+# for i in range(5):
+#     # File generation
+#     df_split = pd.DataFrame(questions[i], columns=["centerpiece", "options", "correct_options", "correct_options_idx", "correct_options_literal", "subject"])
+#     df_split.to_csv(f"mmlu_{i+1}.csv", index=False)
 
-    # Check questions per subject
-    subject_counts = df_split["subject"].value_counts()
-    print(f"Split {i+1} subject counts:\n{subject_counts}\n")
+#     # Check questions per subject
+#     subject_counts = df_split["subject"].value_counts()
+#     print(f"Split {i+1} subject counts:\n{subject_counts}\n")
